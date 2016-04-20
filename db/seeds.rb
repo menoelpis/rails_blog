69ad1dcd67ef2df1ddc18c20338997a6cc201702
Daniel Park <menoelpis@gmail.com>
2016-04-21 05:14:32 +0900

    Added Seed for Moderator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+moderator = Moderator.create(
+    fullname: "Daniel Park",
+    username: "danielpark@gmail.com",
+    password: "danielpark")
